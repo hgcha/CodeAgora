82ffca52f2a903831b022395e435e0de1d447d25
hgcha <ghkdthcha@naver.com>
2023-08-11 13:38:31 +0900

    프로젝트 환경설정

---
+++ src/main/java/hgcha/CodeAgora/CodeAgoraApplication.java
@@ -0,0 +1,13 @@
+package hgcha.CodeAgora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAgoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hgcha/CodeAgora/CodeAgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package hgcha.CodeAgora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
